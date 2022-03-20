ef3c6cf893f31dcaa66537d0d6b2a11b0ae27e05
Kashish Arora <kashisharora252@gmail.com>
2022-03-20 13:30:51 +0530

    Frontend completed

---
+++ frontend/src/products.js
@@ -0,0 +1,74 @@
+const products = [
+    {
+        _id: '1',
+        name: 'book1',
+        image: '/images/book1.jpg',
+        description: 'The Song of Achilles is a 2011 novel by American writer Madeline Miller. Set during the Greek Heroic Age, it is an adaptation of Homers Iliad as told from the perspective of Patroclus.',
+        condition: 'New',
+        price: '200',
+        countInStock: '0',
+        rating: '4.5',
+        numReviews: '4',
+
+    },
+    {
+        _id: '2',
+        name: 'book2',
+        image: '/images/book2.jpg',
+        description:'It Ends With Us is one of the most powerful books of 2016 and one of the most raw, honest, inspiring, and profoundly beautiful stories I have ever read. It was not anything like what I expected, but it delivered so much more than I ever hoped for.',
+        condition: 'New',
+        price: '200',
+        countInStock: '10',
+        rating: '4.5',
+        numReviews: '4',
+
+    },
+    {
+        _id: '3',
+        name: 'book3',
+        image: '/images/book3.jpg',
+        description:'The Stories We Never Tell is a contemporary story that not only revolves around mental health issues, but also the pressures that young people put on themselves. The Surat-based author’s other books include Everybody Has A Story-1, Everybody Has A Story-2, and This is Not Your Story. How did the idea for The Stories We Never Tell come to you?',
+        condition: 'New',
+        price: '220',
+        countInStock: '20',
+        rating: '4',
+        numReviews: '4',
+
+    },
+    {
+        _id: '4',
+        name: 'book4',
+        image: '/images/book4.jpg',
+        description:'Based on the acclaimed novel by Joshua Henkin, The World Without You deals with a grieving family that gathers on the Fourth of July weekend to honor the memory of their journalist son, Leo Frankel, who was killed in Iraq the previous year.',
+        condition: 'readable',
+        price: '150',
+        countInStock: '20',
+        rating: '4',
+        numReviews: '4',
+
+    },
+    {
+        _id: '5',
+        name: 'book5',
+        image: '/images/book5.png',
+        description:'Sometimes pretending to fall in love can be as difficult as actually doing so! At least that is the concept of Alex Light is new book, The Upside of Falling.d on the acclaimed novel by Joshua Henkin, The World Without You deals with a grieving family that gathers on the Fourth of July weekend to honor the memory of their journalist son, Leo Frankel, who was killed in Iraq the previous year.',
+        condition: 'mostly new',
+        price: '120',
+        countInStock: '20',
+        rating: '4',
+        numReviews: '4',
+
+    },
+    {
+        _id: '6',
+        name: 'book6',
+        image: '/images/book6.jpg',
+        description:'Colleen Hooves Ugly Love is an emotional exploration of a woman fighting her attraction for a man determined not to fall in love with her, but she can not seem to understand why he keeps her at arms length.Sometimes pretending to fall in love can be as difficult as actually doing so! At least that is the concept of Alex Light is new book, The Upside of Falling.d on the acclaimed novel by Joshua Henkin, The World Without You deals with a grieving family that gathers on the Fourth of July weekend to honor the memory of their journalist son, Leo Frankel, who was killed in Iraq the previous year.',
+        condition: 'mostly new',
+        price: '130',
+        countInStock: '20',
+        rating: '4',
+        numReviews: '4',
+    }
+]
+export default products
